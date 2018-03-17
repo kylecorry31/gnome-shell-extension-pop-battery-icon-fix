d834cd55de46f6985d1a4dc826819a0bb71b0af3
Kyle Corry <kylecorry31@gmail.com>
2018-03-17 18:16:56 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Pop Battery Icon Fix Gnome Shell Extension
+Fixes the battery icon in Pop!\_OS to correctly match the current battery level.
+
+## Installation
+To install, clone this repo and use make to install
+
+```shell
+$ git clone https://github.com/kylecorry31/gnome-shell-extension-pop-battery-icon-fix
+$ cd gnome-shell-extension-pop-battery-icon-fix
+$ make
+$ make install
+```
+
+The extension then must be enabled using Gnome Tweak Tool or https://extensions.gnome.org/local/
---
+++ extension.js
@@ -0,0 +1,47 @@
+const Main = imports.ui.main;
+const UPower = imports.gi.UPowerGlib;
+
+
+function init() {
+}
+
+function enable() {
+    this.power = Main.panel.statusArea.aggregateMenu._power;
+
+    this.power._oldSync = this.power._sync;
+
+    this.power.getIcon = getIcon;
+
+    this.power._sync = function(){
+      this._oldSync();
+      
+      icon = this.getIcon(this._proxy.Percentage, this._proxy.State == UPower.DeviceState.CHARGING);
+
+      this._indicator.icon_name = icon;
+      this._item.icon.icon_name = icon;
+
+    }
+
+    this.power._sync();
+}
+
+function getIcon(percentage, charging){
+    if (percentage < 10)
+      return charging ? "battery-caution-charging-symbolic" : "battery-caution-symbolic";
+    else if (percentage < 30)
+      return charging ? "battery-low-charging-symbolic" : "battery-low-symbolic";
+    else if (percentage < 60)
+      return charging ? "battery-medium-charging-symbolic" : "battery-medium-symbolic";
+    else if (percentage < 90)
+      return charging ? "battery-good-charging-symbolic" : "battery-good-symbolic";
+    return charging ? "battery-full-charging-symbolic" : "battery-full-symbolic";
+}
+
+
+
+function disable() {
+    this.power._sync = this.power._oldSync;
+    this.power._oldSync = undefined;
+    this.power.getIcon = undefined;
+    this.power._sync();
+}
